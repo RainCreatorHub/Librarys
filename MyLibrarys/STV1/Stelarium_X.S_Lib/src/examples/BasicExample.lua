cf9bdc696699f006d362f81066838caa9dc04121
RainCreatorHub <rain.hub.creator@gmail.com>
2025-08-12 21:09:03 -0300

    Create BasicExample.lua

---
+++ MyLibrarys/STV1/Stelarium_X.S_Lib/src/examples/BasicExample.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(game:HttpGet(""))()
+
+local window = Library:MakeWindow({
+	Name = "Painel Básico"
+})
+
+if window then
+	print("Janela básica criada com sucesso!")
+end
