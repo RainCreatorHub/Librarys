523262f837442babf7b161e8f106035346bece5f
RainCreatorHub <rain.hub.creator@gmail.com>
2025-09-29 22:29:22 -0300

    Update init.lua

--- MyLibrarys/AI/init.lua
+++ MyLibrarys/AI/init.lua
@@ -1,19 +1,14 @@
--- DoorLib/init.lua
 local DoorLib = {}
-
 DoorLib.Components = {
 	Window = require(script.src.Components.Window),
 	Notify = require(script.src.Components.Notify),
 	KeySystem = require(script.src.Components.KeySystem)
 }
-
 function DoorLib:MakeWind(config)
 	config = config or {}
 	return DoorLib.Components.Window.new(config, self)
 end
-
 function DoorLib:Notify(title, desc, time)
 	return DoorLib.Components.Notify.new(title, desc, time)
 end
-
 return DoorLib
