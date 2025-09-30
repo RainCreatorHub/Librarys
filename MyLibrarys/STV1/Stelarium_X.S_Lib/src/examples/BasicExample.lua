2f8bf33d78b795b2f615ae94478cb52ab75d3e21
RainCreatorHub <rain.hub.creator@gmail.com>
2025-09-29 21:26:05 -0300

    Delete MyLibrarys/STV1/Stelarium_X.S_Lib directory

--- MyLibrarys/STV1/Stelarium_X.S_Lib/src/examples/BasicExample.lua
+++
@@ -1,9 +0,0 @@
-local Library = loadstring(game:HttpGet(""))()
-
-local window = Library:MakeWindow({
-	Name = "Painel Básico"
-})
-
-if window then
-	print("Janela básica criada com sucesso!")
-end
--- MyLibrarys/STV1/Stelarium_X.S_Lib/src/lib/init.lua
+++
@@ -1,9 +0,0 @@
-local WindowModule = require(script.components.Window)
-
-local Stelarium_X_S_Lib = {}
-
-function Stelarium_X_S_Lib:MakeWindow(Info)
-	return WindowModule:MakeWindow(Info)
-end
-
-return Stelarium_X_S_Lib
